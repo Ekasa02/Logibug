8f7a46cf350e13238d3be6551a5e0a7815673e60
Varick Tovandy <inter.varick88@gmail.com>
2023-03-30 20:07:10 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HeroCard></HeroCard>
+  </div>
+</template>
+
+<script>
+import HeroCard from '../components/HeroCard.vue';
+
+export default {
+    name: "IndexPage",
+    components: { HeroCard }
+}
+</script>
