1be1a015231434c4e0bf1f18ceb10d0f18fc3453
Ekasa02 <safitrieka636@gmail.com>
2023-03-31 22:02:50 +0700

    create landigpage

---
+++ components/landingpage/TextPage.vue
@@ -0,0 +1,8 @@
+<template>
+        <div class="container mx-auto">
+          <div class="text-center">
+            <h1 class="text-3xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+            <p class="mt-4 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          </div>
+        </div>
+</template>
---
+++ pages/landingpage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <text-page> </text-page>
+        <image-content></image-content>
+        
+    </div>
+   
+</template>
+
+<script>
+    import ImageContent from '../components/landingpage/ImageContent.vue';
+    import TextPage from '../components/landingpage/TextPage.vue';
+    
+export default{
+    components:{
+        ImageContent,
+        TextPage,
+    }
+}
+</script>
