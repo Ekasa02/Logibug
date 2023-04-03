1db0b3c6b3eaa5652e2066384ce0ae6f5ae8d429
Ekasa02 <safitrieka636@gmail.com>
2023-04-03 09:12:06 +0700

    create component for image and teks

--- components/landingpage/TextPage.vue
+++ components/landingpage/TextPage.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="container mx-auto">
           <div class="text-center">
-            <h1 class="text-3xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+            <h1 class="text-2xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
             <p class="mt-4 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
           </div>
         </div>
--- pages/landingpage.vue
+++ pages/landingpage.vue
@@ -1,20 +1,28 @@
 <template>
     <div>
-        <text-page> </text-page>
-        <image-content></image-content>
-        
+      <text-page></text-page>
+      <div class="container ">
+        <div class="flex ">
+          <div >
+            <text-page></text-page>
+          </div>
+          <image-content></image-content>
+        </div>
+      </div>
     </div>
-   
-</template>
-
-<script>
-    import ImageContent from '../components/landingpage/ImageContent.vue';
-    import TextPage from '../components/landingpage/TextPage.vue';
-    
-export default{
-    components:{
-        ImageContent,
-        TextPage,
-    }
-}
-</script>
+  </template>
+  
+  <script>
+  import ImageContent from '../components/landingpage/ImageContent.vue';
+//   import TextBox from '../components/landingpage/TextBox.vue';
+  import TextPage from '../components/landingpage/TextPage.vue';
+  
+  export default {
+    components: {
+      ImageContent,
+    //   TextBox,
+      TextPage,
+    },
+  };
+  </script>
+  
