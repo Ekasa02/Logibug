8e56ffed56d5d657b7575138b605d596c97497b8
Ekasa02 <safitrieka636@gmail.com>
2023-04-04 09:20:55 +0700

    replace bacause conflicts

--- components/landingpage/TextPage.vue
+++ components/landingpage/TextPage.vue
@@ -1,8 +1,5 @@
-<template>
-        <div class="container mx-auto">
-          <div class="text-center">
-            <h1 class="text-2xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-            <p class="mt-4 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-          </div>
-        </div>
+<template>       
+          <div class="text-center w-[632px] h-[96px]">
+            <h1 class="text-2xl font-bold font-[Montserrat]">Lorem ipsum dolor sit amet, <br/> consectetur adipisicing elit</h1>
+          </div>     
 </template>
--- pages/landingpage.vue
+++ pages/landingpage.vue
@@ -1,28 +1,48 @@
 <template>
-    <div>
+  <div class="bg-[#EDF1FF]">
+    <div class="pl-[403px] pr-[405px] pt-[50px]">
       <text-page></text-page>
-      <div class="container ">
-        <div class="flex ">
-          <div >
-            <text-page></text-page>
-          </div>
-          <image-content></image-content>
-        </div>
+    </div>
+    <div class="flex-1 flex align-items-center justify-end">
+      <div class="mr-[70px] mt-[70px]">
+        <text-box></text-box>
+      </div>
+      <div class="ml-[20px] ">
+        <img src="../assets/images/icon/Group 1.svg" alt="logo" class="object-cover" >
       </div>
     </div>
-  </template>
+    <div class="flex-1 flex align-items-center justify-end">
+      <div class="pl-[3px] pr-[100px] pt-2 ">
+        <img src="../assets/images/icon/Group 2.svg" alt="logo" class="object-cover" >
+      </div>
+      <div class="mr-[50px] mt-[70px] pt-[60px]">
+        <text-box></text-box>
+      </div>
+    </div>
+    <div class="flex-1 flex align-items-center justify-end">
+      <div class="mr-[70px] mt-[70px] pt-[60px]">
+        <text-box></text-box>
+      </div>
+      <div class="ml-[20px] mr-[100px] ">
+        <img src="../assets/images/icon/Group 3.svg" alt="logo" class="object-cover" >
+      </div>
+    </div>
+  </div>
   
-  <script>
-  import ImageContent from '../components/landingpage/ImageContent.vue';
-//   import TextBox from '../components/landingpage/TextBox.vue';
+</template>
+
+<script>
+  import TextBox from '../components/landingpage/TextBox.vue';
   import TextPage from '../components/landingpage/TextPage.vue';
   
   export default {
     components: {
-      ImageContent,
-    //   TextBox,
+      TextBox,
       TextPage,
     },
   };
-  </script>
-  
+</script>
+
+<style>
+
+</style>
