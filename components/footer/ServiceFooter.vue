e7135b2b7e2fa6e4f70a7e5ed622e452b8bb659a
Ekasa02 <safitrieka636@gmail.com>
2023-04-06 11:37:07 +0700

    component for social media, service, company, and contact us

---
+++ components/footer/ServiceFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="logo flex ml-1 pt-1">
+            <h1 class="text-[#333333] font-bold text-[20px] ml-[5px] pt-2 w-[180px] h-[50px]">Service</h1>
+        </div>
+        <div class="logo ml-2 pt-2">
+            <p class="text-[#333333] ml-[5px]  w-[180px] ">Manual Testing</p>
+        </div>
+        <div class="logo ml-2 pt-2">
+            <p class="text-[#333333] ml-[5px]  w-[180px] ">API Testing</p>
+        </div>
+        <div class="logo ml-2 pt-2">
+            <p class="text-[#333333] ml-[5px]  w-[180px] ">Testing Management</p>
+        </div>
+        <div class="logo ml-2 pt-2">
+            <p class="text-[#333333] ml-[5px]  w-[180px] ">Test Plan</p>
+        </div>
+        <div class="logo ml-2 pt-2">
+            <p class="text-[#333333] ml-[5px]  w-[180px] ">Bug Report</p>
+        </div>
+    </div>
+</template>
