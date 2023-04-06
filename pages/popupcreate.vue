ec1607808d2009943f90e472439a4aec83ada981
Ekasa02 <safitrieka636@gmail.com>
2023-04-07 01:22:12 +0700

    to display the code imported from the component as a whole

---
+++ pages/popupcreate.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="bg-[#33333385] fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center ">
+        <div class="w-950px h-950px bg-white p-8 rounded-lg shadow-lg">
+            <div>
+                <header-project></header-project>
+            </div>
+            <div>
+                <project-name></project-name>
+            </div>
+            <div>
+                <desc-project></desc-project>
+            </div>
+            <div class="flex pt-[70px] justify-between ">
+                <button-cancel class="ml-auto mr-[15px]"></button-cancel>
+                <button-create></button-create>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import ButtonCancel from '../components/createproject/ButtonCancel.vue';
+import ButtonCreate from '../components/createproject/ButtonCreate.vue';
+import HeaderProject from '../components/createproject/HeaderProject.vue';
+import ProjectName from '../components/createproject/ProjectName.vue';
+import DescProject from '../components/createproject/DescProject.vue';
+
+export default {
+    components:{
+        ButtonCancel,
+        ButtonCreate,
+        HeaderProject,
+        ProjectName,
+        DescProject
+    }
+}
+</script>
