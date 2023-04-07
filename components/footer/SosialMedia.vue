4f30234059d7e81e29ef6f18adb7d01a78a08cd1
Ekasa02 <safitrieka636@gmail.com>
2023-04-07 15:17:32 +0700

    rename file

--- components/footer/SosialMedia.vue
+++ components/footer/SosialMedia.vue
@@ -2,19 +2,19 @@
     <div >
         <div class="logo flex ml-2 pt-2">
             <img src="./svg/Group 21.svg" alt="LogiBug Logo">
-            <h1 class="text-[#333333] text-2xl font-bold ml-[5px] w-[30px] h-[30px]">LogiBug</h1>
+            <h1 class="text-[#333333] text-3xl font-['Nunito'] font-bold ml-[5px] w-[30px] h-[30px]">LogiBug</h1>
         </div>
         <div class="logo flex ml-2 pt-2">
             <img src="./svg/instagram.svg" alt="Instagram Logo">
-            <p class="text-[#333333] ml-[5px] pt-2 w-[30px] h-[30px]">Instagram</p>
+            <p class="text-[#333333] ml-[5px] pt-2 w-[30px] h-[30px] font-['Montserrat']">Instagram</p>
         </div>
         <div class="logo flex ml-2 pt-5">
             <img src="./svg/facebook.svg" alt="Instagram Logo">
-            <p class="text-[#333333] ml-[5px] pt-2 w-[30px] h-[30px]">Facebook</p>
+            <p class="text-[#333333] ml-[5px] pt-2 w-[30px] h-[30px] font-['Montserrat']">Facebook</p>
         </div>
         <div class="logo flex ml-2 pt-5">
             <img src="./svg/twitter-square.svg" alt="Instagram Logo">
-            <p class="text-[#333333] ml-[5px] pt-2 w-[30px] h-[30px]">Twitter</p>
+            <p class="text-[#333333] ml-[5px] pt-2 w-[30px] h-[30px] font-['Montserrat']">Twitter</p>
         </div>
     </div>
     
--- pages/forgotpass.vue
+++ pages/forgotpass.vue
@@ -2,8 +2,8 @@
     <div class="h-screen flex">
       <div class="w-1/3 p-8">
         <LogibugLogo />
-        <forgotpass-title/>
-        <forgotpass-form />
+        <forgot-title/>
+        <forgot-form />
       </div>
       <div class="w-2/3 bg-blue-500"></div>
     </div>
@@ -11,15 +11,15 @@
   
   <script>
   
-  import ForgotpassTitle from '../components/forgotpass/ForgotpassTitle.vue';
-  import ForgotpassForm from '../components/forgotpass/ForgotpassForm.vue';
+  import ForgotTitle from '../components/Forget/ForgotTitle.vue';
+  import ForgotForm from '../components/Forget/ForgotForm.vue';
   import LogibugLogo from '../components/major/LogibugLogo.vue';
   
   export default {
     components: {
       LogibugLogo,
-      ForgotpassTitle,
-      ForgotpassForm,
+      ForgotTitle,
+      ForgotForm,
     }
   }
   </script>
