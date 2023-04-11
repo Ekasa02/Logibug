6c55942d07bb0f5ae196c25660c4794f4751fed6
Ekasa02 <safitrieka636@gmail.com>
2023-04-11 16:31:21 +0700

    fixed code

--- components/changepass/ChangepassForm.vue
+++ components/changepass/ChangepassForm.vue
@@ -54,7 +54,7 @@
         <label
           for="confirmPassword"
           class="absolute text-[#4D4D4D] font-medium duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:-top-1 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:scale-75 peer-focus:-translate-y-2 left-10"
-          >Confirm New Password</label
+          ></label
         >
         <img
           src="../icons/Password.svg"
