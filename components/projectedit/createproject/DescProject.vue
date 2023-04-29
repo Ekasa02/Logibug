4ba21959d7f63cbab49f3d596de8af4558686133
Ekasa02 <safitrieka636@gmail.com>
2023-04-29 21:39:21 +0700

    add to component for popup

--- components/projectedit/createproject/DescProject.vue
+++ components/projectedit/createproject/DescProject.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="font-['Montserrat'] pt-2">
-      <label class="block text-gray-700 text-[18px] font-bold mb-2" for="description">
-        Description 
-      </label>
-      <textarea
+  <div class="font-['Montserrat'] pt-[15px]">
+    <label
+      class="block text-gray-700 text-[14px] font-bold mb-2"
+      for="description"
+    >
+      Description
+    </label>
+    <textarea
       id="description"
-        class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        placeholder="Description your project"
-      ></textarea>
-    </div>
-  </template>
-
-
-  
+      class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      placeholder="Description your project"
+    ></textarea>
+  </div>
+</template>
---
+++ components/projectedit/createproject/DropPlatform.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pt-[15px] relative" >
+      <label class="block font-['Montserrat'] font-bold text-[14px] mb-2" for="project-name">
+        Platform
+      </label>
+      <div class="flex items-center">
+        <input
+          id="project-name"
+          class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+          type="text"
+          placeholder="Platform"
+        >
+        <img src="./svg/DropDown.svg" class="absolute right-2">
+      </div>
+    </div>
+  </template>
+  
