3fbc3bd5734f7dd232a87a47585b5bca631666fc
Ekasa02 <safitrieka636@gmail.com>
2023-05-04 16:32:57 +0700

    Replace code

---
+++ components/projectedit/testcase/PopupTestcase.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-[#33333385] fixed top-0 left-0 right-0 bottom-0 flex items-center sm:justify-center font-['monserrat']">
+    <div>
+      <div class="bg-white rounded-lg shadow-lg">
+        <header-create></header-create>
+        <div class="pl-5 pr-5">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderCreate from './HeaderCreate.vue';
+
+
+export default {
+  components: {
+    HeaderCreate,
+    
+  },
+}
+</script>
---
+++ components/version/deleteversion/ButtonDelete.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="font-['Montserrat'] bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ">
+            Delete
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
