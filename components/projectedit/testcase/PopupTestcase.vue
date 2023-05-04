dcccbe0ee601a77985383690385f4fa3f0aebed2
Ekasa02 <safitrieka636@gmail.com>
2023-05-04 18:08:06 +0700

    Create components for test case

--- components/projectedit/testcase/PopupTestcase.vue
+++ components/projectedit/testcase/PopupTestcase.vue
@@ -4,7 +4,15 @@
       <div class="bg-white rounded-lg shadow-lg">
         <header-create></header-create>
         <div class="pl-5 pr-5">
-          
+          <scenario-test></scenario-test>
+          <test-category></test-category>
+          <pre-condition></pre-condition>
+          <test-case></test-case>
+          <test-step></test-step>
+          <expect-test></expect-test>
+          <div class="pt-6">
+            <button-create></button-create>
+          </div>
         </div>
       </div>
     </div>
@@ -12,13 +20,25 @@
 </template>
 
 <script>
+import ButtonCreate from '../createproject/ButtonCreate.vue';
 import HeaderCreate from './HeaderCreate.vue';
-
+import ScenarioTest from './ScenarioTest.vue';
+import TestCategory from './TestCategory.vue';
+import PreCondition from './PreCondition.vue';
+import TestCase from './TestCase.vue';
+import TestStep from './TestStep.vue';
+import ExpectTest from './ExpectTest.vue';
 
 export default {
   components: {
     HeaderCreate,
-    
+    ScenarioTest,
+    TestCategory,
+    PreCondition,
+    TestCase,
+    TestStep,
+    ExpectTest,
+    ButtonCreate,
   },
 }
 </script>
---
+++ components/projectedit/testcase/ScenarioTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class=" relative">
+      <label
+        class="block font-['Montserrat'] font-bold text-[14px] mb-2"
+        for="scenario"
+      >
+        Scenario
+      </label>
+      <div class="flex items-center">
+        <input
+          id="scenario"
+          class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Scenario"
+        />
+        <img
+          src=".././createproject/svg/DropDown.svg"
+          class="absolute right-2"
+        />
+      </div>
+  </div>
+</template>
