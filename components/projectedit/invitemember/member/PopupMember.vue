eb449f4424eea628d05319f64277427990421109
Ekasa02 <safitrieka636@gmail.com>
2023-05-04 17:22:30 +0700

    create popup member

---
+++ components/projectedit/invitemember/member/PopupMember.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bg-[#33333385] fixed top-0 left-0 right-0 bottom-0 flex items-center sm:justify-center font-['monserrat']">
+      <div>
+        <div class="bg-white rounded-lg shadow-lg ">
+          <header-member></header-member>
+          <div class="pl-5 pr-5">
+            <div>
+              <div class="flex">
+                <profile-name></profile-name>
+                <edit-member></edit-member>
+              </div>
+              <hr class=" border-gray-300 my-4 w-full">
+              <div class="flex">
+                <profile-name></profile-name>
+                <edit-member></edit-member>
+              </div>
+              <hr class=" border-gray-300 my-4 w-full">
+              <div class="flex">
+                <profile-name></profile-name>
+                <edit-member></edit-member>
+              </div>
+              <hr class=" border-gray-300 my-4 w-full">
+              <div class="flex">
+                <profile-name></profile-name>
+                <edit-member></edit-member>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import HeaderMember from './HeaderMember.vue';
+ import ProfileName from './ProfileName.vue';
+ import EditMember from './EditMember.vue';
+  
+  export default {
+    components: {
+      HeaderMember,
+      ProfileName,
+      EditMember,
+    },
+  }
+  </script>
+  
--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
         <!-- <popup-testcase></popup-testcase> -->
-        <popup-log></popup-log>
+        <!-- <popup-log></popup-log> -->
+        <popup-member></popup-member>
     </div>
 </template>
 
 <script>
 // import PopupTestcase from "../components/projectedit/testcase/PopupTestcase.vue";
-import PopupLog from '../components/loginform/logout/PopupLog.vue';
+// import PopupLog from '../components/loginform/logout/PopupLog.vue';
+import PopupMember from '../components/projectedit/invitemember/member/PopupMember.vue';
 
 
 export default{
     components:{
         // PopupTestcase,
-        PopupLog,
+        // PopupLog,
+        PopupMember,
     }
 }
 </script>
