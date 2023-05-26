2748bdf8646dd8effb711a1bd3e82fdc5a91992e
Varick Tovandy <inter.varick88@gmail.com>
2023-05-26 11:40:40 +0700

    commit jumat

---
+++ components/detailtestcase/DetailHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="flex items-center justify-end py-[14px] pr-[100px] border-b-[1px] border-gray-300">
+            <div class="ml-auto">
+                <LogibugProfile @click="showPopup = true" />
+            </div>
+        </div>
+        <DashboardPopup v-if="showPopup" @close="showPopup = false" />
+    </div>
+</template>
+  
+<script>
+import LogibugProfile from '../major/LogibugProfile.vue';
+
+export default {
+    components: { LogibugProfile },
+    data() {
+        return {
+            showPopup: false
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
