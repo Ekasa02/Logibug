f4e9cb4e9394dad5bb1ff85fe7f0cb55fbb72ec4
Varick Tovandy <inter.varick88@gmail.com>
2023-05-28 07:30:47 +0700

    commit varick

--- components/loginform/logout/PopupLog.vue
+++ components/loginform/logout/PopupLog.vue
@@ -1,21 +1,21 @@
 <template>
-    <div @click="showLogout" class="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-[#33333385]">
-      <div class="bg-white p-8 rounded-lg shadow-lg">
-        <div class="pl-[400px]">
-          <img src="../.././assets/CloseCircle.svg" alt="image">
-        </div>
-        <div class="pt-6 pl-11 pr-7">
-          <img src="../../assets/Logout logo.svg" alt="image">
-          <h1 class="text-center pt-7 font-['Montserrat'] font-bold">Logout Account?</h1>
-          <p class="text-center font-['Montserrat']">Are you sure you want to logout the account</p>
-        </div>
-        <div class="flex pt-[30px] justify-center items-center">
-          <button-no class="mr-[19px]"></button-no>
-          <button-yes></button-yes>
-        </div>
+  <div @click="showLogout" class="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-[#33333385]">
+    <div class="bg-white p-8 rounded-lg shadow-lg">
+      <div>
+        <img src="../.././assets/CloseCircle.svg" alt="image">
+      </div>
+      <div class="pt-6 pl-11 pr-7">
+        <img src="../../assets/Logout logo.svg" alt="image">
+        <h1 class="text-center pt-7 font-['Montserrat'] font-bold">Logout Account?</h1>
+        <p class="text-center font-['Montserrat']">Are you sure you want to logout the account</p>
+      </div>
+      <div class="flex pt-[30px] justify-center items-center">
+        <button-no class="mr-[19px]"></button-no>
+        <button-yes></button-yes>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <script>
 import ButtonNo from "./ButtonNo.vue";
