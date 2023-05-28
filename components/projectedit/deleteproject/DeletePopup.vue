9c80b394977944a522a8d919bd2710cb2270fdc2
Ekasa02 <safitrieka636@gmail.com>
2023-05-28 23:43:38 +0700

    popup profil

--- components/projectedit/deleteproject/DeletePopup.vue
+++ components/projectedit/deleteproject/DeletePopup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click="deleteProject" class="bg-[#33333385] fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center ">
+    <div class="bg-[#33333385] fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center " @click="deleteProject" >
         <div class="w-950px h-950px bg-white p-8 rounded-lg shadow-lg">
             <div class="pl-[360px]">
                 <img src="../svg/CloseCircle.svg" alt="image" >
